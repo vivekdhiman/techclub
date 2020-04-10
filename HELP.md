45ffb92942b52b7719aab9fa20aeeff3be168415
Vivek Dhiman <vivek.dhiman1@gmail.com>
2020-04-10 15:29:30 -0600

    Commit Template

--- HELP.md
+++ HELP.md
@@ -14,3 +14,4 @@ The following guides illustrate how to use some features concretely:
 * [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
 * [Building REST services with Spring](https://spring.io/guides/tutorials/bookmarks/)
 
+   ./mvnw spring-boot:run -s /Users/vivekd/AEM/apache-maven-3.5.3/conf/settings.xml
---
+++ src/main/java/com/contentful/demo/constants/SystemConstants.java
@@ -0,0 +1,10 @@
+package com.contentful.demo.constants;
+
+public class SystemConstants {
+    private static final String SPACE_ID = "i1zlrs8ok9t8";
+    private static final String ACCESS_TOKEN = "CFPAT-lQlGc-phnLlCHJF2HyNOgFbrCeY6evxd593THhXNaGs";
+
+    private static final String CONTENT_TYPE_PRODUCT = "2PqfXUJwE8qSYKuM0U6w8M";
+    private static final String CONTENT_TYPE_BRAND = "sFzTZbSuM8coEwygeUYes";
+    private static final String CONTENT_TYPE_CATEGORY = "6XwpTaSiiI2Ak2Ww0oi6qa";
+}
